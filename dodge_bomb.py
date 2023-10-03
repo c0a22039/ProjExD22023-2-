a11ffd919647256d968d6654c8ed26c62c80978a
Yurika <c0a22039@edu.teu.ac.jp>
2023-10-03 15:22:27 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,13 @@ delta={
     pg.K_RIGHT:(+5,0),
 }
 
+def check_bound(obj_rct:pg.Rect):
+    yoko,tate=True,True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+        yoko=False
+    if obj_rct.top<0 or HEIGHT <obj_rct.bottom:
+        tate=False
+    return yoko,tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -38,6 +45,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bd_rct):
+            print("ゲームオーバー")
+            return 
 
         screen.blit(bg_img, [0, 0])
 
@@ -48,11 +58,21 @@ def main():
             if key_lst[key]:
                 sum_mv[0]+=mv[0]
                 sum_mv[1]+=mv[1]
-        
         kk_rct.move_ip(sum_mv[0],sum_mv[1])
-
-
+        if check_bound(kk_rct)!=(True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
+
+
+
+        bd_rct.move_ip(vx,vy)
+        yoko,tate=check_bound(bd_rct)
+        if not yoko:
+            vx*=-1
+        if not tate:
+            vy*=-1
+        screen.blit(bd_img, bd_rct)
+
 
         #爆弾
         bd_rct.move_ip(vx,vy)
